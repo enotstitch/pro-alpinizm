1e6b87f8ab37cbf21fd2073cc65e1bdd1452bcd8
Anna Kessler <kessler.anna96@gmail.com>
2025-01-14 17:50:17 +0300

    Вёрстка, правки, адаптация многих блоков

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,7 @@
 import { initHeader } from './modules/header/header.js';
 import { Tabs } from './scripts/tabs.js';
+import { lettersSwiper, technicSwiper } from './scripts/sliders.js';
+import { skillsProgressInit } from './scripts/skillsProgress.js';
 
 initHeader();
 new Tabs(
@@ -9,3 +11,8 @@ new Tabs(
 	'active',
 	'grid',
 );
+
+skillsProgressInit();
+
+lettersSwiper.init();
+technicSwiper.init();
