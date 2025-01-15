5c5f75c40f9283014a2b5fa226c08057482278b6
Konstantin <mogilevkostya@yandex.ru>
2025-01-15 16:46:35 +0700

    Вёрстка секции с ценами, адаптация главного экрана

--- assets/js/scripts/sliders.js
+++ assets/js/scripts/sliders.js
@@ -63,3 +63,19 @@ export const reviewsSwiper = new Swiper('.reviews__swiper', {
 		prevEl: '.swiper-button-prev',
 	},
 });
+
+export const examplesSwiper = new Swiper('.examples__swiper', {
+	modules: [Navigation],
+
+	init: false,
+	direction: 'horizontal',
+	loop: false,
+	slidesPerView: 3,
+	spaceBetween: 40,
+	centeredSlides: false,
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
